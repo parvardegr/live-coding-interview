4d6f2e1192b57ae9b23270518d396067df0d55de
Danial Eskandari <doneskandari@gmail.com>
2024-08-26 01:44:10 +0330

    Refactor: Add concurrency param equal to partition.count for parallel processing

--- src/main/java/ir/metrix/interview/MessageConsumer.java
+++ src/main/java/ir/metrix/interview/MessageConsumer.java
@@ -5,10 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-//TODO: maximise the consumer performance in terms of how many message it can process per second
 @Component
 public class MessageConsumer {
     private static final Logger LOG = LoggerFactory.getLogger(MessageConsumer.class.getName());
@@ -21,9 +21,11 @@ public class MessageConsumer {
 
     //TODO: metric for throughput (to show how many message per seconds we can handle)
     @KafkaListener(
-            topics = "${topic.name}"
-    )  //TODO: Adjust kafka listener configuration
-    public void consume(Message message) {  //TODO: use batch poll
+            topics = "${topic.name}",
+            concurrency = "${partition.count}"
+//            batch = "true"
+    )
+    public void consume(Message message) {
 
         this.service.processMessage(message);
 
--- src/main/java/ir/metrix/interview/MessageProcessorService.java
+++ src/main/java/ir/metrix/interview/MessageProcessorService.java
@@ -5,7 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class MessageProcessorService {
@@ -15,6 +17,7 @@ public class MessageProcessorService {
     @Value("${message.count}")
     private int messageCount;
     private final AtomicInteger processedCount = new AtomicInteger(0);
+    private final AtomicLong startTime = new AtomicLong(-1);
     private final Object lock = new Object();
 
     private final UserService userService;
@@ -29,12 +32,16 @@ public class MessageProcessorService {
         String phoneNumber = userService.fetchPhoneById(message.getUserId());
         smsService.sendMessage(phoneNumber, message);
 
-        synchronized (lock) {  //TODO: remove later
-            if (processedCount.get() == 0)
+        synchronized (lock) {
+            if (processedCount.get() == 0) {
+                startTime.set(System.currentTimeMillis());
                 LOG.info("Start Processing...");
+            }
 
-            if (processedCount.incrementAndGet() >= messageCount)
-                LOG.info("Finish Processing Messages. count={}", processedCount);
+            if (processedCount.incrementAndGet() >= messageCount) {
+                long elapsedTime = System.currentTimeMillis() - startTime.get();
+                LOG.info("Finish Processing Messages. count={}, elapsedTime={} ms", processedCount.get(), elapsedTime);
+            }
         }
     }
 
