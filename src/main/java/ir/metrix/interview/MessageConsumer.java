6b7bc61433b878a531ce2fb00f1f288d95b6e75c
Danial Eskandari <doneskandari@gmail.com>
2024-08-26 14:12:16 +0330

    Refactor: Measure the process duration in MessageConsumer class

--- src/main/java/ir/metrix/interview/MessageConsumer.java
+++ src/main/java/ir/metrix/interview/MessageConsumer.java
@@ -1,12 +1,25 @@
 package ir.metrix.interview;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Component
 public class MessageConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MessageConsumer.class.getName());
+
+    @Value("${message.count}")
+    private int messageCount;
+    private final AtomicLong startTime = new AtomicLong(-1);
+    private final AtomicInteger processedCount = new AtomicInteger(0);
+    private final Object lock = new Object();
 
     private final MessageProcessorService service;
 
@@ -24,6 +37,17 @@ public class MessageConsumer {
 
         this.service.processMessages(messages);
 
+        synchronized (lock) {
+            if (processedCount.get() == 0) {
+                startTime.set(System.currentTimeMillis());
+                LOG.info("Start Processing...");
+            }
+
+            if (processedCount.addAndGet(messages.size()) >= messageCount) {
+                long elapsedTime = System.currentTimeMillis() - startTime.get();
+                LOG.info("Finish Processing Messages. count={}, elapsedTime={} ms", processedCount.get(), elapsedTime);
+            }
+        }
     }
 
 }
--- src/main/java/ir/metrix/interview/MessageProcessorService.java
+++ src/main/java/ir/metrix/interview/MessageProcessorService.java
@@ -1,24 +1,11 @@
 package ir.metrix.interview;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class MessageProcessorService {
-
-    private static final Logger LOG = LoggerFactory.getLogger(MessageProcessorService.class.getName());
-
-    @Value("${message.count}")
-    private int messageCount;
-    private final AtomicInteger processedCount = new AtomicInteger(0);
-    private final AtomicLong startTime = new AtomicLong(-1);
-    private final Object lock = new Object();
 
     private final UserService userService;
     private final SmsService smsService;
@@ -31,35 +18,11 @@ public class MessageProcessorService {
     public void processMessage(Message message) {
         String phoneNumber = userService.fetchPhoneById(message.getUserId());
         smsService.sendMessage(phoneNumber, message);
-
-        synchronized (lock) {
-            if (processedCount.get() == 0) {
-                startTime.set(System.currentTimeMillis());
-                LOG.info("Start Processing...");
-            }
-
-            if (processedCount.incrementAndGet() >= messageCount) {
-                long elapsedTime = System.currentTimeMillis() - startTime.get();
-                LOG.info("Finish Processing Messages. count={}, elapsedTime={} ms", processedCount.get(), elapsedTime);
-            }
-        }
     }
 
     public void processMessages(List<Message> messages) {
         String phoneNumber = userService.fetchPhoneById(messages.get(0).getUserId());
         smsService.sendMessages(phoneNumber, messages);
-
-        synchronized (lock) {
-            if (processedCount.get() == 0) {
-                startTime.set(System.currentTimeMillis());
-                LOG.info("Start Processing...");
-            }
-
-            if (processedCount.addAndGet(messages.size()) >= messageCount) {
-                long elapsedTime = System.currentTimeMillis() - startTime.get();
-                LOG.info("Finish Processing Messages. count={}, elapsedTime={} ms", processedCount.get(), elapsedTime);
-            }
-        }
     }
 
 }
